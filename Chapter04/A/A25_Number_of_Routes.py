b7d4ac984216ab70a45dde6fed0268dd7f2bbda2
ymsk-sky <ymsk.sky.95@gmail.com>
2024-06-04 20:47:48 +0900

    Chapter4 A25

---
+++ Chapter04/A/A25_Number_of_Routes.py
@@ -0,0 +1,13 @@
+h, w = map(int, input().split())
+cl = [input() for _ in range(h)]
+dp = [[0] * w for _ in range(h)]
+dp[0][0] = 1
+for i in range(h):
+    for j in range(w):
+        if cl[i][j] == "#":
+            continue
+        if i > 0:
+            dp[i][j] += dp[i - 1][j]
+        if j > 0:
+            dp[i][j] += dp[i][j - 1]
+print(dp[h - 1][w - 1])
